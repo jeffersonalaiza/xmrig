2b57764db43579158f81e133a98c1103d5224ea1
Jefferson Alaiza <73538875+jeffersonalaiza@users.noreply.github.com>
2021-04-05 21:06:14 +0800

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install git -y
-git clone https://github.com/itsgelogomayee/xmrig
+git clone https://github.com/jeffersonalaiza/xmrig
 cd xmrig
 chmod u+x xmrig
 screen -S xmrig
